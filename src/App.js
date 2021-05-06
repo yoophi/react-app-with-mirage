31d0535190c29cad72420975f9d5b5f27148bfd4
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-06 15:50:04 +0900

    ✨ IMPROVE: add namespace

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { createServer } from "miragejs";
 
 createServer({
   routes() {
-    this.get("/api/users", () => [
+    this.namespace = 'api'
+
+    this.get("/users", () => [
       { id: "1", name: "Luke" },
       { id: "2", name: "Leia" },
       { id: "3", name: "Anakin" },
