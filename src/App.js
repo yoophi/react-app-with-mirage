9540b3211e16e824f834d1a5cd4a825cc7c31427
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-06 15:27:37 +0900

    ✨ NEW: add simple miragejs routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createServer } from "miragejs";
 
-function App() {
+createServer({
+  routes() {
+    this.get("/api/users", () => [
+      { id: "1", name: "Luke" },
+      { id: "2", name: "Leia" },
+      { id: "3", name: "Anakin" },
+    ]);
+  },
+});
+export function App() {
+  let [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
