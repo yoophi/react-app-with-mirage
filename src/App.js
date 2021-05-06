c23fd7c934090a623243b12868113e1e4d2751e7
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-06 17:18:06 +0900

    ✨ NEW: apply movie fixtures and pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { createServer, Model } from "miragejs";
+import movies from "./fixtures/movies.json";
 
+function paginate(array, pageNumber, pageSize) {
+  return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+}
 createServer({
   models: {
     movie: Model,
   },
-
+  seeds(server) {
+    server.db.loadData({
+      movies: movies,
+    });
+  },
   routes() {
     this.namespace = "api";
 
@@ -14,13 +22,19 @@ createServer({
       { id: "2", name: "Leia" },
       { id: "3", name: "Anakin" },
     ]);
-    this.get("/movies", () => {
+    this.get("/movies", (schema, request) => {
+      const {
+        page: pageNumber = 1,
+        per_page: pageSize = 10,
+      } = request.queryParams;
       return {
-        movies: [
-          { id: 1, name: "Inception", year: 2010 },
-          { id: 2, name: "Interstellar", year: 2014 },
-          { id: 3, name: "Dunkirk", year: 2017 },
-        ],
+        data: paginate(schema.db.movies, Number(pageNumber), Number(pageSize)),
+        pagination: {
+          page: Number(pageNumber),
+          per_page: Number(pageSize),
+          total: schema.db.movies.length,
+          total_page: Math.ceil(schema.db.movies.length / Number(pageSize)),
+        },
       };
     });
     this.get("/httpbin/:id", (schema, request) => {
@@ -31,6 +45,7 @@ createServer({
     });
   },
 });
+
 export function App() {
   let [users, setUsers] = useState([]);
   let [movies, setMovies] = useState([]);
@@ -40,7 +55,7 @@ export function App() {
     fetch("/api/users")
       .then((response) => response.json())
       .then((json) => setUsers(json));
-    fetch("/api/movies")
+    fetch("/api/movies?per_page=25&page=2")
       .then((response) => response.json())
       .then((json) => setMovies(json));
     fetch("/api/httpbin/42?foo=bar&hello=world")
